cab27a2c710635a04fa97a8640fa4de4a9e891b3
projectmazdoor <75509191+projectmazdoor@users.noreply.github.com>
2022-01-12 06:17:56 -0600

    Added missing return type

--- azanSchedular.py
+++ azanSchedular.py
@@ -1,13 +1,16 @@
+import logging
 from datetime import datetime
-
+import pygame
 import schedule
 import time
 import requests
 import json
 import webbrowser
 
-headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
-response = requests.get('https://api.aladhan.com/v1/timingsByCity?city=woburn&country=United%20Arab%20Emirates&method=12', headers=headers)
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+response = requests.get(
+    'https://api.aladhan.com/v1/timingsByCity?city=woburn&country=United%20Arab%20Emirates&method=12', headers=headers)
 
 fajar = '00:11'
 zuhar = '00:12'
@@ -15,21 +18,29 @@ asar = '00:13'
 magrib = '00:14'
 isha = '00:15'
 
+
 # this job runs once a day for latest time of azan
 def getAzanApi():
-    print("----getAzanApi-----",)
+    logging.basicConfig(filename='app.log',
+                        level=logging.DEBUG)
+    print("----getAzanApi-----", )
+    logging.debug('----getAzanApi-----')
 
     now = datetime.now()
     current_time = now.strftime("%H:%M")
     print("Current Time =", current_time)
+    logging.debug("Current Time ="+ current_time)
     print('Azan Time Api Scheduled NOW !')
+    logging.debug('Azan Time Api Scheduled NOW !')
     if response.status_code == 200:
         print('Success!')
+        logging.debug('Success!')
 
         json_data = json.loads(response.text)
         # print(json_data)
         fileData = json_data["data"]['timings']
         print(json_data["data"]['timings'])
+        logging.debug(json_data["data"]['timings'])
 
         # writing data of azan in a file
         with open('azanData.txt', 'w') as outfile:
@@ -40,55 +51,54 @@ def getAzanApi():
             magrib = json_data["data"]['timings']['Maghrib']
             isha = json_data["data"]['timings']['Isha']
             print("Fajar %s Zuhar %s Asar %s Maghrib %s Isha %s" % (fajar, zuhar, asar, magrib, isha))
-            # working
+            logging.debug("Fajar %s Zuhar %s Asar %s Maghrib %s Isha %s" % (fajar, zuhar, asar, magrib, isha))
+
+            # play azan for fajar
+            schedule.every().day.at(str(fajar)).do(playAzan)
+
+            # play azan for zuhar
+            schedule.every().day.at(str(zuhar)).do(playAzan)
+
+            # play azan for asar
+            schedule.every().day.at(str(asar)).do(playAzan)
+
+            # play azan for maghrib
+            schedule.every().day.at(str(magrib)).do(playAzan)
+
+            # play azan for isha
+            schedule.every().day.at(str(isha)).do(playAzan)
+            # working in window but not in linux
             # webbrowser.open("azan.mp3")
+            # wavFile = input("azan.mp3")
+            # working in both window and linux
+            # pygame.mixer.init()
+            # pygame.mixer.music.load("azan.mp3")
+            # pygame.mixer.music.play()
     elif response.status_code == 404:
         print('Not Found.')
+        logging.debug('Not Found.')
+
     print("------------", )
+    logging.debug("------------")
+    return
 
-    # play azan for fajar
-    schedule.every().day.at(str(fajar)).do(playAzan)
-
-    # play azan for zuhar
-    schedule.every().day.at(str(zuhar)).do(playAzan)
-
-    # play azan for asar
-    schedule.every().day.at(str(asar)).do(playAzan)
-
-    # play azan for maghrib
-    schedule.every().day.at(str(magrib)).do(playAzan)
-
-    # play azan for isha
-    schedule.every().day.at(str(isha)).do(playAzan)
 
 
 def playAzan():
     print("playAzan")
+    logging.debug("playAzan")
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("azan.mp3")
+    pygame.mixer.music.play()
+    return schedule.CancelJob
     # working
-    webbrowser.open("azan.mp3")
-
-def job3():
-    print("Hello")
-
+    # webbrowser.open("azan.mp3")
 
 # this job runs once a day for latest time of azan
-schedule.every().day.at("00:00").do(getAzanApi)
-#
-#
-# # play azan for fajar
-# schedule.every().day.at(str(fajar)).do(playAzan)
-#
-# # play azan for zuhar
-# schedule.every().day.at(str(zuhar)).do(playAzan)
-#
-# # play azan for asar
-# schedule.every().day.at(str(asar)).do(playAzan)
-#
-# # play azan for maghrib
-# schedule.every().day.at(str(magrib)).do(playAzan)
-#
-# # play azan for isha
-# schedule.every().day.at(str(isha)).do(playAzan)
+schedule.every().day.at("07:28").do(getAzanApi)
+
+
 
 
 # schedule.every().hour.do(job)
